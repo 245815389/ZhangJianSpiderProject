6695aa324b03b73369c788fa42d720dbf3fa1db8
zhangjian <245815389@qq.com>
2020-04-12 18:55:00 +0800

    Add files via upload
    part1  招聘信息爬取

---
+++ zhilian/zhilian/pipelines.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+from urllib import request
+import pymongo
+
+class ZhilianPipeline(object):
+    def open_spider(self, spider):
+        self.f = open("zhaopin.json", "a", encoding="utf-8")
+
+    def process_item(self, item, spider):
+        self.f.write(json.dumps(dict(item), indent=4, ensure_ascii=False) + ',\n')
+        return item
+
+    def close_spider(self, spider):
+        self.f.close()
+
+
+class MonPipeline(object):
+    def open_spider(self,spider):
+        self.conn= pymongo.MongoClient("localhost", 27017)
+        self.db = self.conn.zhaopindb
+        self.tables = self.db.zhaopintable
+
+    def process_item(self, item, spider):
+        self.tables.insert(dict(item))
+        return item
+
+    def close_spider(self,spider):
+        self.conn.close()
---
+++ zhilian/zhilian/spiders/zhaopin.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from .. items import ZhilianItem
+
+class ZhaopinSpider(scrapy.Spider):
+    name = 'zhaopin'
+    allowed_domains = ['xiaoyuan.zhaopin.com']
+    start_urls = ['https://xiaoyuan.zhaopin.com/search/jt=9000300000000&pg=1']
+
+    def parse(self, response):
+        div_list = response.xpath('//div[starts-with(@id,"pane-reletive")]/div/div')
+        for div in div_list:
+            item = ZhilianItem()
+            name = div.xpath('.//div[@class="fn-left position"]/text()').extract_first()
+            city = div.xpath('.//span[@class="city fn-left"]/text()').extract_first()
+            company = div.xpath('.//div[@class="fn-right company"]/text()').extract_first()
+
+            item["name"] = name
+            item["city"] = city
+            item["company"] = company
+            yield item
+
+            base_url = "https://xiaoyuan.zhaopin.com/search/jt=9000300000000&pg=%s"
+
+            for i in range(1, 2):
+                full_url = base_url % i
+                yield scrapy.Request(url=full_url, callback=self.parse)
+
