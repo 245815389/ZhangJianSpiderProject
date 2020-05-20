cc372814671be41aa23fd93ff9bf21519b524cae
zhangjian <245815389@qq.com>
2020-05-21 00:08:14 +0800

    Add files via upload
    最终版2.0

---
+++ fangyuan/pipelines.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+import json
+from urllib import request
+import pymongo
+
+class FangyuanPipeline(object):
+    def open_spider(self, spider):
+        self.f = open("fangyuan.json", "a", encoding="utf-8")
+
+    def process_item(self, item, spider):
+        self.f.write(json.dumps(dict(item), indent=4, ensure_ascii=False) + ',\n')
+        return item
+
+    def close_spider(self, spider):
+        self.f.close()
+
+class MonPipeline(object):
+    def open_spider(self,spider):
+        self.conn= pymongo.MongoClient("localhost", 27017)
+        self.db = self.conn.fangyuandb
+        self.tables = self.db.fangyuantable
+        self.tables.remove({})
+
+    def process_item(self, item, spider):
+        self.tables.insert(dict(item))
+        return item
+
+    def close_spider(self,spider):
+        self.conn.close()
